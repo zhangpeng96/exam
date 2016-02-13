9421c812cb0accd36bd4c5f7b62256f501ab0d63
ZhangPeng <sjiaomail@gmail.com>
2016-02-13 20:34:03 +0800

    Using localstorage to store messages

--- program.js
+++ program.js
@@ -1,13 +1,25 @@
+/*
+    四角鸣墨题库(New Version)
+    version: 3.0.2
+    date:  2016/2/13 20:29
+*/
 $(function(){
   /*
     全局CSS配置
   */
-
   $('.nav-tabs > li').css({'float':'none','margin-bottom':'0'});
-
+  $(".next").hide();
+  $(".panel").hide();
+  /*
+    全局载入脚本
+  */
+    //当前位置获得
+  pos = 0;
+  
+  timer('start');
 
   /*
-    选择、填空题布尔验证
+    选择、填空题布尔验证函数
   */
   function validate(input1,input2,type){
     var str1 = $.trim(input1);
@@ -41,63 +53,120 @@ $(function(){
       }
     }
   }
-
   /*
-    当前位置获得
+    题目成绩计算函数
   */
-  pos = 0;
+  function calcScore(inputData){
+    if(!inputData){
+      return 0;
+    }else{
+      return 1.0;
+    }
+  }
+
 
   /*
     下一题触发事件
   */
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     // 进入下一题
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     var activeTab = $(e.target).attr('next-pos');
 
     // 声明全局变量pos
     pos = $(e.target).attr('open-pos');
-    localStorage.qPos = pos; 
+    localStorage.qPos = pos;
+
+    // 更改并载入进度条动画
+    $(".progress-animate").remove();
+    $(".fill-header").after("<div id=\"progress\" class=\"progress-back progress-animate\" <\/div>");
     $(".progress-animate").css({
-      "animation-name:":"process",
-      "animation-timing-function": "linear",
-      "animation-duration": "30s"
+      "animation-name":"process",
+      "animation-duration": msg[pos-1].time+"s"
     });
+    var timeOut = (durationRecord>msg[pos-1].time)?true:false;
+    // 添加post数据
+    postLocal.data[pos] =  {
+                "time": timeRecord,
+                "duration": durationRecord,
+                "frequency": 3,
+                "isHint": gloHint,
+                "isTimeout": timeOut,
+                "result": gloResult,
+                "score": calcScore(gloResult),
+                "postAnswer": gloPost
+            }
 
-    //$(".progress-animate").stop();
+    // 将post数据存入localStorage
+    localStorage.setItem("postData",JSON.stringify(postLocal));
 
-    /*$(".progress-animate").remove();
-    $(".fill-header").after("<div id=\"progress\" class=\"progress-back progress-animate\" style=\"animation-duration\"\:\""+String(msg[pos].time)+"s\"><\/div>");*/
-    //$(".progress-animate").attr("animation-duration", String(msg[pos].time)+'s');
-    /*$(".progress-animate").css({
-      "animation-name:":"clear",
-      "animation-name:":"process",
-      "animation-duration":"30s"
-    });*/
-    // 更改进度条动画
-    //$(".progress-animate").attr("animation-duration", "30s");
-    //$(".progress-animate").removeAttr("animation-name").attr("animation-name", "process");
-   
-   /*removeClass("progress-animate").addClass("progress-animate");
-    $("#progress").removeClass("progress-animate");
-    $("#progress").addClass("progress-animate");*/
-   /* $(".progress-animate").attr("animation-duration", String(msg[pos].time));
-    alert("okl");*/
-
+    // 开始下一题的计时
+    timer('start');
+    gloHint = 0;
   });
 
-
+  $("#getTip").click(function(){
+    $(".tab-pane:eq("+pos+")").find(".panel").show();
+    gloHint = 12;
+  })
+/**/
   /*
     进入历史定位
   */
   if (!localStorage.qPos){
-
   }else if(localStorage.qPos==1){
-
-
   }else{
-    $(".tab-pane").removeClass("active");
-    $(".tab-pane:eq("+localStorage.qPos+")").addClass("in active");
+    $(".tab-pane").removeClass("active");   //禁用首页active
+    $(".tab-pane:eq("+localStorage.qPos+")").addClass("in active"); //获得local数据并定位显示
   }
+
+
+
+
+/*
+  选择题判断机制
+*/
+
+    $(".tab-pane li").click(function(){
+      // 计时停止
+      timer('stop');
+      //
+      // 由于选择器是在页面加载时就已经确定的，所以无法使用变量pos
+      var input = $(this).attr('opt-order');
+      gloPost = input;
+      // 显示「下一题」按钮
+      $(this).parent().parent().find('.next').show();     
+
+      // 题目result返回数据
+      if(validate(input, msg[pos-1].answer, msg[pos-1].validate)){
+        $(this).addClass('answerTrue');
+        gloResult = true;
+      }else{
+        $(this).addClass('answerFalse');
+        $(this).siblings('[opt-order=\"'+msg[pos-1].answer+'\"]').addClass('answerTrue');
+        gloResult = false;
+      }
+      // 进度条暂停
+      $(".progress-animate").css({
+        "animation-play-state":"paused"
+      });
+
+    });
+
+
+  /*
+    计时函数
+  */
+  function timer(step){
+    var getTime = parseInt(new Date().getTime()/1000);
+    if(step=='start'){
+      timeRecord = getTime;
+    }else if(step=='stop'){
+      durationRecord = (getTime-timeRecord);
+    }else if(step=='clear'){
+      timeRecord = 0;
+    }
+  }
+
 
 
   /*
@@ -130,59 +199,19 @@ $(function(){
     }
 ];
 
-msg_str = JSON.stringify(msg);//将JSON对象转化成字符串
-localStorage.setItem("str",msg_str);
-
-  /*	$("div").click(function(){
-  alert(msg[pos].uid);
-      var str1 = $("#data1").val();
-      var str2 = $("#data2").val();
-      var type = $("#data3").val();
-      alert(validate('A a','Aa','space'));*/
-/*  });
-
-    $.getJSON("msg.json",function(result){
-        $(".active-tab").append(result[3].validate + "|");
-      $.each(result, function(i, field){
-        $(".active-tab").append(field.validate + " ");
-      });
-    });
-*/
-
-/*
-  选择题判断机制
-*/
-
-    $(".tab-pane li").click(function(){
-      // 由于选择器是在页面加载时就已经确定的，所以无法使用变量pos
-      var input = $(this).attr('opt-order');
-      if(validate(input, msg[pos-1].answer, msg[pos-1].validate)){
-        $(this).addClass('answerTrue');
-      }else{
-        $(this).addClass('answerFalse');
-        $(this).siblings('[opt-order=\"'+msg[pos-1].answer+'\"]').addClass('answerTrue');
-      }
-    });
-
-
-/*
-  计时函数
-*/
-  function timer(step){
-    var getTime = parseInt(new Date().getTime()/1000);
-    if(step=='start'){
-      timeRecord = getTime;
-    }else if(step=='stop'){
-      timeRecord = (getTime-timeRecord);
-    }else if(step=='clear'){
-      timeRecord = 0;
-    }
-    return timeRecord;
+  /*
+    local储存
+  */
+  var postLocal = {
+      "config": [
+          {
+              "postUID": 555555
+          }
+      ],
+      "data": [
+      ]
   }
 
-/*
-  local储存
-*/
 
 
 
@@ -195,28 +224,6 @@ localStorage.setItem("str",msg_str);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-  timer('clear');
-  $("#btn-start").click(function(){
-    timer('start');
-    $("#data").val(timeRecord);
-  });
-  $("#btn-stop").click(function(){
-    timer('stop');    
-    $("#data").val(timeRecord);
-  });
-*/
 
 
 
@@ -249,3 +256,51 @@ function debug(){
 
 
 });
+
+
+/*
+{
+    "config": [
+        {
+            "postUID": 555555
+        }
+    ],
+    "data": [
+        {
+            "time": 15533722,
+            "duration": 26,
+            "frequency": 3,
+            "isHint": 4,
+            "isTimeout": true,
+            "result": true,
+            "score": 0.9,
+            "postAnswer": "4"
+        },
+        {
+            "time": 15533722,
+            "duration": 26,
+            "frequency": 3,
+            "isHint": 4,
+            "isTimeout": true,
+            "result": true,
+            "score": 0.9,
+            "postAnswer": "4"
+        }
+    ]
+}
+
+  $("div").click(function(){
+  alert(msg[pos].uid);
+      var str1 = $("#data1").val();
+      var str2 = $("#data2").val();
+      var type = $("#data3").val();
+    alert(validate('A a','Aa','space'));
+  });
+
+    $.getJSON("msg.json",function(result){
+        $(".active-tab").append(result[3].validate + "|");
+      $.each(result, function(i, field){
+        $(".active-tab").append(field.validate + " ");
+      });
+    });
+*/
