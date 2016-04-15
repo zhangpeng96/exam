59b7c30b42983a719a43db69c5646bfe613962b1
ZhangPeng <sjiaomail@gmail.com>
2016-04-15 23:54:54 +0800

    Added closure

--- program.js
+++ program.js
@@ -1,11 +1,243 @@
 /*
-	四角鸣墨题库(New Version)
-	version: 3.2.0
-	date:  2016/4/13 18:39
+    四角鸣墨题库(New Version)
+    version: 3.2.1
+    date:  2016/4/14 23:59
 */
 $(function() {
 
 /*
+    数据函数
+*/
+  
+  /* 闭包函数实现的私有变量调用 */
+function accessData(){
+  var entry = {
+      uid: 1,
+      time: 140000000,
+      duration: 40,
+      frequency: 2,
+      isHint: 0,
+      isTimeout: 1,
+      result: 1,
+      score: 1.0,
+      postAnswer: 'your answer'
+  };
+  var reserve;
+  var status = {
+      type: 1,
+      pos: 0,
+      count: 0
+  };
+  var temp;
+  return {
+      inital: function(){
+
+      },
+      entClear: function(){
+
+      },
+      entSet: function(key, value){
+
+      },
+      entRead: function(key){
+          switch(key){
+            case 'uid':
+              return entry.uid;
+            break;
+            case 'time':
+              return entry.time;
+            break;
+            case 'duration':
+              return entry.duration;
+            break;
+            case 'frequency':
+              return entry.frequency;
+            break;
+            case 'isHint':
+              return entry.isHint;
+            break;
+            case 'isTimeout':
+              return entry.isTimeout;
+            break;
+            case 'result':
+              return entry.result;
+            break;
+            case 'score':
+              return entry.score;
+            break;
+            case 'postAnswer':
+              return entry.postAnswer;
+            break;
+            default:
+              return -1;
+          }
+      },
+      posInc: function(){
+          status.pos ++;
+      },  //位置标志递增
+      posDec: function(){
+          status.pos --;
+      },  //位置标志递减
+      posRead: function(){
+          return status.pos;
+      }   //读取位置标志
+  }
+  // 闭包结构
+}
+
+var e = accessData();
+e.posInc();
+console.log(e.posRead());
+console.log(e.entRead('postAnswer'));
+  
+  /* 获得localStorage.qPos数据 */
+  function getHistoryPos() {
+    return localStorage.qPos;
+  }
+  
+  /* 设置localStorage.qPos数据 */
+  function setHistoryPos(data) {
+    localStorage.qPos = parseInt(data);
+  }
+  
+  /* 写入localStorage题目数据 */
+  function writeStorage() {
+    postLocal.data[pos] = {
+      "time": timeRecord,
+      "duration": durationRecord,
+      "frequency": 3,
+      "isHint": 0,
+      //isHint算法计算不出来
+      "isTimeout": timeOut,
+      "result": gloResult,
+      "score": calcScore(gloResult),
+      "postAnswer": gloPost
+    }
+    localStorage.setItem("postData", JSON.stringify(postLocal));
+  }
+  
+  /* 创建复杂localStorage数据 */
+  function createStorage() {
+    var postLocal = {
+      "config": [{
+        "postUID": 555555
+      }],
+      "data": []
+    }
+  }
+  
+  /* 初始化localStorage数据 */
+  function intialStorage() {}
+
+  
+/*
+    功能函数
+*/
+  
+  /* 单项成绩计算 */
+  function calcScore(inputData) {
+    if (!inputData) {
+      return 0;
+    } else {
+      return 1.0;
+    }
+  }
+  
+  /* 计时器 */  
+  function timer(step, record) {
+    var now = parseInt(new Date().getTime() / 1000);
+    if (step == 'start') {
+      var time = now;
+    } else if (step == 'stop') {
+      var time = (now - record);
+    } else if (step == 'clear') {
+      var time = 0;
+    } else {
+      var time = -1;
+    }
+    return time;
+  }
+  
+  /* 检测是否超时 */
+  function isTimeout(input, plan) {
+    if (input > plan) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  /*  选择、填空题布尔验证函数   */
+  function validate(input1, input2, type) {
+    var str1 = $.trim(input1);
+    var str2 = $.trim(input2);
+    function delSpace(str) {
+      return str.replace(/\s+/g, "");
+    }
+    if (type == 'exact') {
+      if (str1 == str2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else if (type == 'case') {
+      if (str1.toLowerCase() == str2.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else if (type == 'space') {
+      if (delSpace(str1) == delSpace(str2)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else if (type == 'sc') {
+      var data1 = delSpace(str1.toLowerCase());
+      var data2 = delSpace(str2.toLowerCase());
+      if (data1 == data2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+/*
+    Layout函数
+*/
+
+  /* 初始化全局布局 */
+  function intialLayout() {
+    $('.nav-tabs > li').css({
+      'float': 'none',
+      'margin-bottom': '0'
+    });
+    $(".next").hide();
+    $(".panel").hide();
+  }
+  
+  /* 更新进度条动画 */
+  function refreshProgress(time) {
+    $(".progress-animate").remove();
+    $(".fill-header").after("<div id=\"progress\" class=\"progress-animate\" <\/div>");
+    $(".progress-animate").css({
+      "animation-name": "process",
+      "animation-duration": time + "s"
+    });
+  }
+  
+  /* 控制进度条动画动作 */
+  function controlProgress(action) {
+    if (action == 'pause') {
+      $(".progress-animate").css({
+        "animation-play-state": "paused"
+      });
+    } else if (action == 'play') {} else if (action == 'replay') {} else {
+      return -1;
+    }
+  }
+=======
 	数据函数
 */
   
@@ -179,6 +411,7 @@ $(function() {
       return -1;
     }
   }
+>>>>>>> origin/master
   
   /* 显示提示面板 */
   function showHint(pos) {
